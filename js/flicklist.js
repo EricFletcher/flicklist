37664c8d47eccff2ffe2a9af35e729e3d4009359
Eric Fletcher <Eric Fletcher>
2016-10-24 18:35:45 -0500

    finish FlickList 2 studio

--- js/flicklist.js
+++ js/flicklist.js
@@ -14,7 +14,7 @@ var model = {
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "8e888fa39ec243e662e1fb738c42ae99" // TODO 0 add your api key
+  token: "d053f8ffa77e33f7c7af346f3f30a1f4" // TODO 0 add your api key
 }
 
 
@@ -46,13 +46,21 @@ function discoverMovies(callback) {
  * the callback function that was passed in
  */
 function searchMovies(searchTerm, callback) {
-  console.log("searching for movies with '" + searchTerm + "' in their title...");
-
+  //console.log("searching for movies with '" + searchTerm + "' in their title...");
+$.ajax({
+  url: api.root + "/search/movie",
+  data: {
+    api_key: api.token,
+    query: searchTerm
+  },
+    success: function(response){
+    model.browseItems=response.results;
+    callback(response);
+  }
+});
   // TODO 9
   // implement this function as described in the comment above
   // you can use the body of discoverMovies as a jumping off point
-
-
 }
 
 
@@ -70,8 +78,11 @@ function render() {
     var title = $("<p></p>").text(movie.original_title);
     var itemView = $("<li></li>")
       .append(title)
+      .attr("class", "item-watchlist");
       // TODO 3
       // give itemView a class attribute of "item-watchlist"
+      //("itemView").setAttribute("class: item-watchlist");
+
 
     $("#section-watchlist ul").append(itemView);
   });
@@ -84,23 +95,26 @@ function render() {
       .click(function() {
         model.watchlistItems.push(movie);
         render();
-      });
+      })
       // TODO 2
       // the button should be disabled if this movie is already in
       // the user's watchlist
       // see jQuery .prop() and Array.indexOf()
+      .prop("disabled", model.watchlistItems.indexOf(movie) !== -1);
+
 
 
     // TODO 1
     // create a paragraph containing the movie object's .overview value
     // then, in the code block below,
     // append the paragraph in between the title and the button
-
+      var overview= $("<p></p>").text(movie.overview);
 
     // append everything to itemView, along with an <hr/>
     var itemView = $("<li></li>")
       .append($("<hr/>"))
       .append(title)
+      .append(overview)
       .append(button);
 
     // append the itemView to the list
